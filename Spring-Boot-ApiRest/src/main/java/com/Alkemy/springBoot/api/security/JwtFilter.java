01b5188e7d820f579a9ae794da50451dfe3b7443
Jonatan Fontana <fontanajonatan1133@gmail.com>
2021-08-06 18:17:01 -0300

    update_security

--- Spring-Boot-ApiRest/src/main/java/com/Alkemy/springBoot/api/security/JwtFilter.java
+++ Spring-Boot-ApiRest/src/main/java/com/Alkemy/springBoot/api/security/JwtFilter.java
@@ -1,4 +1,59 @@
 package com.Alkemy.springBoot.api.security;
 
-public class JwtFilter {
+//Se ejecuta en cada peticion y por medio de jwtPovider si es valido permite el acceso.
+
+import com.Alkemy.springBoot.api.security.service.UserDetailServiceImp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(JwtFilter.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    UserDetailServiceImp userDetailServiceImp;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = getToken(request);
+            if (token != null && jwtProvider.validateToken(token)){
+
+                String userName = jwtProvider.getFirstNameFromToken(token);
+                UserDetails userDetails = userDetailServiceImp.loadUserByUsername(userName);
+
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(userDetails,null,
+                                userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+
+        }catch (Exception e){
+            LOGGER.error("Error  en el metodo doFilterInternal");
+        }
+            filterChain.doFilter(request,response);
+    }
+
+    private String getToken(HttpServletRequest request){
+
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer"))
+            return header.replace("Bearer ","");
+        return null;
+    }
 }
--- Spring-Boot-ApiRest/src/main/java/com/Alkemy/springBoot/api/security/JwtProvider.java
+++ Spring-Boot-ApiRest/src/main/java/com/Alkemy/springBoot/api/security/JwtProvider.java
@@ -1,4 +1,58 @@
 package com.Alkemy.springBoot.api.security;
 
+//Genera token , valida que no este mal formado ni expirado.
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtProvider {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    public String generateToken(Authentication authentication){
+
+        UserDetailClass userDetailClass = (UserDetailClass) authentication.getPrincipal();
+
+        return Jwts.builder().setSubject(userDetailClass.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512,secret).compact();
+    }
+
+    public String getFirstNameFromToken(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJwt(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJwt(token);
+            return true;
+
+        }catch (MalformedJwtException e){
+            LOGGER.error("token mal formado");
+        }catch (UnsupportedJwtException e){
+            LOGGER.error("token no soportado");
+        }catch (ExpiredJwtException e){
+            LOGGER.error("token expired");
+        }catch (IllegalArgumentException e){
+            LOGGER.error("token vacio");
+        }catch (SignatureException e){
+            LOGGER.error("fallo en la firma");
+        }
+        return false;
+    }
+
 }
